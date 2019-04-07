a3c0564e21644b63dbb08aee07d1ba6cfbe79b84
dmsch <dschukin@mail.ru>
2019-04-07 21:01:42 +0300

    clients distribution for full allocated rooms

--- api/helpers/general/distribute-clients.js
+++ api/helpers/general/distribute-clients.js
@@ -9,14 +9,14 @@ module.exports = {
 
   inputs: {
     oldRoom: {
-      friendlyName: 'oldRoom',
-      description: 'oldRoom',
+      friendlyName: 'oldRoom number',
+      description: 'oldRoom number',
       type: 'number',
       required: true,
     },
     newRoom: {
-      friendlyName: 'newRoom',
-      description: 'newRoom',
+      friendlyName: 'newRoom number',
+      description: 'newRoom number',
       type: 'number',
       required: true,
     },
@@ -37,23 +37,42 @@ module.exports = {
     sails.log.debug('general:distributeClients helper...');
     sails.log.debug('input params: ', inputs);
 
+    const oldRoomWithClients = await Room.findOne({room: inputs.oldRoom})
+      .populate('client');
+    const newRoomWithClients = await Room.findOne({room: inputs.newRoom});
+
+    // sails.log.warn('oldRoomWithClients: ', oldRoomWithClients);
+    // sails.log.warn('newRoomWithClients: ', newRoomWithClients);
 
     try {
 
-      // const oldRoomId = await Room.findOne({room: inputs.oldRoom}).id;
-      // const newRoomId = await Room.findOne({room: inputs.newRoom}).id;
-      //
-      // const oldRoomClients = await Client.find({
-      //   where: {
-      //     deleted: false,
-      //     banned: false,
-      //     service_subscription_finalized: true,
-      //     subscription_active: true,
-      //     rooms: inputs.oldRoom
-      //   },
-      //   select: ['id', 'guid', 'rooms'],
-      // });
+      _.forEach(oldRoomWithClients.client, async function (clientRec) {
 
+        // sails.log.warn('clientRec: ', clientRec);
+
+        if (
+          !clientRec.deleted
+          && !clientRec.banned
+          && !clientRec.admin
+          && clientRec.service_subscription_finalized
+          && clientRec.subscription_active
+        ) {
+
+          if (_.random(0,1)) {
+
+            // sails.log.info('Gonna re-allocate this client: ', clientRec);
+
+            await Client.removeFromCollection(clientRec.id, 'room', oldRoomWithClients.id);
+            await Client.addToCollection(clientRec.id, 'room', newRoomWithClients.id);
+            await Room.updateOne({room: oldRoomWithClients.room})
+              .set({clients_number: oldRoomWithClients.clients_number - 1});
+            await Room.updateOne({room: newRoomWithClients.room})
+              .set({clients_number: newRoomWithClients.clients_number + 1});
+          }
+
+        }
+
+      });
 
       return exits.success({
         status: 'ok',
