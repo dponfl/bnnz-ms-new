8700f3390f6d886ec9d900d401a931fea2c86bf2
dmsch <dschukin@mail.ru>
2019-05-04 19:26:41 +0300

    implement help::add_account_start block

--- api/helpers/funnel/help/callback-add_account.js
+++ api/helpers/funnel/help/callback-add_account.js
@@ -1,10 +1,10 @@
 module.exports = {
 
 
-  friendlyName: 'help::callbackStart',
+  friendlyName: 'help::callbackAddAccount',
 
 
-  description: 'help::callbackStart',
+  description: 'help::callbackAddAccount',
 
 
   inputs: {
@@ -47,9 +47,7 @@ module.exports = {
 
     try {
 
-      const currentAccount = _.find(inputs.client.accounts, {guid: inputs.client.account_use});
-
-      sails.log.debug('/*************** help::callbackStart ***************/');
+      sails.log.debug('/*************** help::callbackAddAccount ***************/');
 
       // sails.log.debug('Client: ', inputs.client);
       sails.log.debug('Block: ', inputs.block);
@@ -57,32 +55,10 @@ module.exports = {
 
 
       switch (inputs.query.data) {
-        case 'help_send_post':
 
-          inputs.block.next = 'help::send_post';
+        case 'help_add_account_yes':
 
-          /**
-           * Update help::send_post block
-           */
-
-          updateBlock = 'help::send_post';
-
-          splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
-          updateFunnel = splitRes[0];
-          updateId = splitRes[1];
-
-
-          getBlock = _.find(inputs.client.funnels[updateFunnel], {id: updateId});
-
-          if (getBlock) {
-
-            getBlock.previous = 'help::start';
-
-          } else {
-
-            throw new Error(`Wrong block decoding for data: ${updateBlock}`);
-
-          }
+          inputs.block.next = 'help::get_login';
 
           inputs.block.done = true;
 
@@ -97,26 +73,11 @@ module.exports = {
 
           break;
 
-        case 'help_add_account':
-
-          inputs.block.next = 'help::add_account_start';
-
-          inputs.block.done = true;
-
-          await sails.helpers.funnel.afterHelperGeneric.with({
-            client: inputs.client,
-            block: inputs.block,
-            msg: inputs.query,
-            next: true,
-            previous: true,
-            switchFunnel: true,
-          });
-
-          break;
-
-        case 'help_return':
+        case 'help_add_account_no':
 
           inputs.block.next = inputs.block.previous;
+          inputs.block.enabled = false;
+          inputs.block.done = false;
 
           /**
            * Update block specified at 'previous' key
@@ -136,36 +97,12 @@ module.exports = {
             getBlock.enabled = true;
             getBlock.done = false;
             getBlock.next = null;
-            getBlock.switchToFunnel = null;
-            inputs.block.switchToFunnel = updateFunnel;
 
           } else {
 
             throw new Error(`Wrong block decoding for data: ${updateBlock}`);
 
           }
-
-          inputs.block.done = true;
-
-          /**
-           * Save the current funnel at performed_funnels table
-           */
-
-          await sails.helpers.storage.performedFunnelsSave.with({
-            client_guid: inputs.client.guid,
-            current_funnel: inputs.client.current_funnel,
-            funnel_data: inputs.client.funnels,
-          });
-
-          /**
-           * Update Help funnel to the initial state to enable the client to perform it again
-           */
-
-          await sails.helpers.general.loadInitialFunnels.with({
-            client: inputs.client,
-            clientCategory: currentAccount.service.funnel_name,
-            funnelName: 'help',
-          });
 
           await sails.helpers.funnel.afterHelperGeneric.with({
             client: inputs.client,
@@ -185,8 +122,8 @@ module.exports = {
     } catch (e) {
 
       throw {err: {
-          module: 'api/helpers/funnel/help/callback-start',
-          message: 'api/helpers/funnel/help/callback-start error',
+          module: 'api/helpers/funnel/help/callback-add_account',
+          message: 'api/helpers/funnel/help/callback-add_account error',
           payload: {
             params: inputs,
             error: {
