ef9dafe6f1e7544d8c4e924a7a20bbb4eec8b925
dmsch <dschukin@mail.ru>
2019-05-04 21:50:37 +0300

    implement help::wrong_profile

---
+++ api/helpers/funnel/help/after-wrong-profile.js
@@ -0,0 +1,108 @@
+module.exports = {
+
+
+  friendlyName: 'help::afterWrongProfile',
+
+
+  description: 'help::afterWrongProfile',
+
+
+  inputs: {
+    client: {
+      friendlyName: 'client',
+      description: 'Client record',
+      type: 'ref',
+      required: true,
+    },
+    block: {
+      friendlyName: 'block',
+      description: 'Current funnel block',
+      type: 'ref',
+      required: true,
+    },
+    msg: {
+      friendlyName: 'message',
+      description: 'Message received',
+      type: 'ref',
+      // required: true,
+    },
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+
+    err: {
+      description: 'Error',
+    }
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    let getLoginBlock;
+
+    try {
+
+      sails.log.debug('/*************** help::afterWrongProfile ***************/');
+
+      let splitRes = _.split(inputs.block.previous, sails.config.custom.JUNCTION, 2);
+      let nextFunnel = splitRes[0];
+      let nextId = splitRes[1];
+
+
+      getLoginBlock = _.find(inputs.client.funnels[nextFunnel], {id: nextId});
+
+      if (getLoginBlock) {
+        getLoginBlock.shown = false;
+        getLoginBlock.done = false;
+        getLoginBlock.next = null;
+
+      }
+
+      await sails.helpers.funnel.afterHelperGeneric.with({
+        client: inputs.client,
+        block: inputs.block,
+        msg: inputs.msg,
+        next: true,
+        previous: false,  // if we do not set it to false then previous block is set done=true
+                          // and we will not be able to move to it again (but we want to move there
+        switchFunnel: true,
+      });
+
+
+    } catch (e) {
+
+      throw {err: {
+          module: 'api/helpers/funnel/help/after-wrong-profile',
+          message: 'api/helpers/funnel/help/after-wrong-profile error',
+          payload: {
+            params: inputs,
+            error: {
+              name: e.name || 'no error name',
+              message: _.truncate(e.message, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error message',
+              stack: _.truncate(e.stack, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error stack',
+              code: e.code || 'no error code',
+            }
+          }
+        }
+      };
+
+    }
+
+
+    return exits.success({
+      status: 'ok',
+      message: 'Success',
+      payload: {}
+    });
+  }
+
+
+};
+
