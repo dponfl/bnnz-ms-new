a67c1bd5912b9ce5db85ac174b8015e296337fc7
dmsch <dschukin@mail.ru>
2019-05-05 19:08:44 +0300

    profile_exists update

---
+++ api/helpers/funnel/help/callback-confirm-profile.js
@@ -0,0 +1,115 @@
+module.exports = {
+
+
+  friendlyName: 'optin::callbackConfirmProfile',
+
+
+  description: 'optin::callbackConfirmProfile',
+
+
+  inputs: {
+    client: {
+      friendlyName: 'client',
+      description: 'Client record',
+      type: 'ref',
+      required: true,
+    },
+    block: {
+      friendlyName: 'block',
+      description: 'Current funnel block',
+      type: 'ref',
+      required: true,
+    },
+    query: {
+      friendlyName: 'query',
+      description: 'Callback query received',
+      type: 'ref',
+      required: true,
+    },
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+
+    err: {
+      description: 'Error',
+    }
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const currentAccount = _.find(inputs.client.accounts, {guid: inputs.client.account_use});
+    const currentAccountInd = _.findIndex(inputs.client.accounts, (o) => {
+      return o.guid === currentAccount.guid;
+    });
+
+    try {
+
+      sails.log.debug('/*************** optin::callbackConfirmProfile ***************/');
+
+      // sails.log.debug('Client: ', inputs.client);
+      sails.log.debug('Block: ', inputs.block);
+      sails.log.debug('Query: ', inputs.query);
+
+
+      switch (inputs.query.data) {
+        case 'profile_confirm_yes':
+          inputs.client.accounts[currentAccountInd].profile_confirmed = true;
+          inputs.block.next = 'optin::we_have_several_service_levels';
+          break;
+        case 'profile_confirm_no':
+          inputs.block.next = 'optin::try_again';
+          break;
+        default:
+          throw new Error(`Wrong callback data: ${inputs.query.data}`);
+      }
+
+      inputs.block.done = true;
+
+      await sails.helpers.funnel.afterHelperGeneric.with({
+        client: inputs.client,
+        block: inputs.block,
+        msg: inputs.query,
+        next: true,
+        previous: true,
+        switchFunnel: true,
+      });
+
+
+    } catch (e) {
+
+      throw {err: {
+          module: 'api/helpers/funnel/optin/callback-confirm-profile',
+          message: 'api/helpers/funnel/optin/callback-confirm-profile error',
+          payload: {
+            params: inputs,
+            error: {
+              name: e.name || 'no error name',
+              message: _.truncate(e.message, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error message',
+              stack: _.truncate(e.stack, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error stack',
+              code: e.code || 'no error code',
+            }
+          }
+        }
+      };
+
+    }
+
+
+    return exits.success({
+      status: 'ok',
+      message: 'Success',
+      payload: {}
+    });
+  }
+
+
+};
+
