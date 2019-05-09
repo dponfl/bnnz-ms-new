67e8552bcaef9195b3373d74aaf49a07fec89ac3
dmsch <dschukin@mail.ru>
2019-05-09 14:33:56 +0300

    try_again block

---
+++ api/helpers/funnel/help/after-wrong-profile-response.js
@@ -0,0 +1,151 @@
+module.exports = {
+
+
+  friendlyName: 'help::afterWrongProfileResponse',
+
+
+  description: 'help::afterWrongProfileResponse',
+
+
+  inputs: {
+    client: {
+      friendlyName: 'client',
+      description: 'Client record',
+      type: 'ref',
+      required: true,
+    },
+    block: {
+      friendlyName: 'block',
+      description: 'Current funnel block',
+      type: 'ref',
+      required: true,
+    },
+    msg: {
+      friendlyName: 'message',
+      description: 'Message received',
+      type: 'ref',
+      // required: true,
+    },
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+  },
+
+
+  fn: async function (inputs,exits) {
+
+    let updateBlock;
+    let getBlock;
+    let splitRes;
+    let updateFunnel;
+    let updateId;
+
+    try {
+
+      sails.log.debug('/*************** help::afterWrongProfileResponse ***************/');
+
+      /**
+       * Update help::get_login block
+       */
+
+      updateBlock = 'help::get_login';
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+
+      getBlock = _.find(inputs.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.shown = false;
+        getBlock.done = false;
+        getBlock.next = null;
+      }
+
+      /**
+       * Update help::confirm_profile block
+       */
+
+      updateBlock = 'help::confirm_profile';
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+
+      getBlock = _.find(inputs.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.enabled = false;
+        getBlock.shown = false;
+        getBlock.done = false;
+        getBlock.next = null;
+      }
+
+      /**
+       * Update help::wrong_profile block
+       */
+
+      updateBlock = 'help::wrong_profile';
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+
+      getBlock = _.find(inputs.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.enabled = false;
+        getBlock.shown = false;
+        getBlock.done = false;
+      }
+
+      await sails.helpers.funnel.afterHelperGeneric.with({
+        client: inputs.client,
+        block: inputs.block,
+        msg: inputs.msg,
+        next: true,
+        previous: false,  // if we do not set it to false then previous block is set done=true
+                          // and we will not be able to move to it again (but we want to move there
+        switchFunnel: true,
+      });
+
+
+    } catch (e) {
+
+      throw {err: {
+          module: 'api/helpers/funnel/help/after-wrong-profile-response',
+          message: 'api/helpers/funnel/help/after-wrong-profile-response error',
+          payload: {
+            params: inputs,
+            error: {
+              name: e.name || 'no error name',
+              message: _.truncate(e.message, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error message',
+              stack: _.truncate(e.stack, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error stack',
+              code: e.code || 'no error code',
+            }
+          }
+        }
+      };
+
+    }
+
+
+    return exits.success({
+      status: 'ok',
+      message: 'Success',
+      payload: {}
+    });
+  }
+
+
+};
+
