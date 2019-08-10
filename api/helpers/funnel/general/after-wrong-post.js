1b1ae108f17a7d1a1bef9af8f6dd87ff457a8663
dmsch <dschukin@mail.ru>
2019-08-10 18:08:14 +0300

    dev/d-4: Generic:General:wrong_post

---
+++ api/helpers/funnel/general/after-wrong-post.js
@@ -0,0 +1,100 @@
+module.exports = {
+
+  friendlyName: 'general::afterWrongPost',
+
+  description: 'general::afterWrongPost',
+
+  inputs: {
+    client: {
+      friendlyName: 'client',
+      description: 'Client record',
+      type: 'ref',
+      required: true,
+    },
+    block: {
+      friendlyName: 'block',
+      description: 'Current funnel block',
+      type: 'ref',
+      required: true,
+    },
+    msg: {
+      friendlyName: 'message',
+      description: 'Message received',
+      type: 'ref',
+    },
+  },
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+  },
+
+  fn: async function (inputs,exits) {
+
+    let updateBlock;
+    let getBlock;
+    let splitRes;
+    let updateFunnel;
+    let updateId;
+
+    try {
+
+      sails.log.debug('/*************** general::afterWrongPost ***************/');
+
+      /**
+       * Update general::make_post
+       */
+
+      updateBlock = 'general::make_post';
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+
+      getBlock = _.find(inputs.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.shown = false;
+        getBlock.done = false;
+        getBlock.next = null;
+      }
+
+      await sails.helpers.funnel.afterHelperGeneric.with({
+        client: inputs.client,
+        block: inputs.block,
+        msg: inputs.msg,
+        next: true,
+        previous: false,  // if we do not set it to false then previous block is set done=true
+                          // and we will not be able to move to it again (but we want to move there
+        switchFunnel: true,
+      });
+    } catch (e) {
+
+      throw {err: {
+          module: 'api/helpers/funnel/general/after-wrong-post',
+          message: 'api/helpers/funnel/general/after-wrong-post error',
+          payload: {
+            params: inputs,
+            error: {
+              name: e.name || 'no error name',
+              message: _.truncate(e.message, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error message',
+              stack: _.truncate(e.stack, {length: sails.config.custom.ERROR_MSG_LENGTH}) || 'no error stack',
+              code: e.code || 'no error code',
+            }
+          }
+        }
+      };
+    }
+
+    return exits.success({
+      status: 'ok',
+      message: 'Success',
+      payload: {}
+    });
+  }
+};
+
