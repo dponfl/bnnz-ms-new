48166310f03d7539270bc08a4e9fc30963c7eb25
dmsch <dschukin@mail.ru>
2019-08-18 20:00:26 +0300

    imp/i-2: use onMissing function on emojify

--- api/helpers/funnel/proceed-next-block.js
+++ api/helpers/funnel/proceed-next-block.js
@@ -545,7 +545,7 @@ function parseSpecialTokens(clientRec, clientName, msg, lang) {
   const numberOfMessagesSentToday = currentAccount.posts_made_day;
   resultStr = _.replace(resultStr, '$PostsSent$', numberOfMessagesSentToday);
 
-  resultStr = emoji.emojify(resultStr);
+  resultStr = emoji.emojify(resultStr, () => '');
 
   return resultStr;
 }
