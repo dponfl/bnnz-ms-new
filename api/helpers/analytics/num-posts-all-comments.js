0ea0c4184edf509aa7f304be08ffe3933aefcbf5
dmsch <dschukin@mail.ru>
2019-12-14 13:49:53 +0200

    analytics: num-posts-all-comments

---
+++ api/helpers/analytics/num-posts-all-comments.js
@@ -0,0 +1,100 @@
+"use strict";
+
+const moment = require('moment');
+
+const moduleName = 'analytics:numPostsAllComments';
+
+
+module.exports = {
+
+
+  friendlyName: 'analytics:numPostsAllComments',
+
+  description: 'Calculate data for num_posts_all_comments event',
+
+  inputs: {
+    start: {
+      friendlyName: 'start',
+      description: 'calculation interval start date & time',
+      type: 'string',
+      required: true,
+    },
+    end: {
+      friendlyName: 'end',
+      description: 'calculation interval end date & time',
+      type: 'string',
+      required: true,
+    },
+  },
+
+  exits: {
+
+    success: {
+      description: 'All done.',    },
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    sails.log.info(`******************** ${moduleName} at ${moment().format()} ********************`);
+
+    let elapsedTimeStart;
+    let elapsedTimeEnd;
+    let elapsedTime;
+    let numPosts;
+
+    try {
+
+      elapsedTimeStart = moment();
+
+      numPosts = await Posts.count({
+        where: {
+          createdAt: {
+            '>=': moment(inputs.start).format(),
+            '<=': moment(inputs.end).format()
+          },
+          all_comments_done: true,
+        },
+      });
+
+      elapsedTimeEnd = moment();
+
+      elapsedTime = moment.duration(elapsedTimeEnd.diff(elapsedTimeStart)).asSeconds();
+
+      // sails.log.info(`Start: ${moment(inputs.start).format()}`);
+      // sails.log.info(`End: ${moment(inputs.end).format()}`);
+      // sails.log.info(`numPosts: ${numPosts}`);
+      // sails.log.info(`elapsedTime: ${elapsedTime}`);
+
+      return exits.success({
+        status: 'ok',
+        message: 'Success',
+        payload: {
+          value: numPosts,
+          elapsedTime: elapsedTime,
+        }
+      });
+
+    } catch (e) {
+
+      const errorLocation = moduleName;
+      const errorMsg = `${moduleName}: General error`;
+
+      sails.log.error(errorLocation + ', error: ' + errorMsg);
+      sails.log.error(errorLocation + ', error details: ', e);
+
+      throw {err: {
+          module: errorLocation,
+          message: errorMsg,
+          payload: {},
+        }
+      };
+
+    }
+
+  } // fn
+
+
+};
+
