935e6e57f9ad2940b7050f87a1a9b0cf28afc49d
dmsch <dschukin@mail.ru>
2020-01-05 13:46:08 +0200

    analytics: accounts-deleted-test-commercial-total

---
+++ api/helpers/analytics/accounts-deleted-test-commercial-total.js
@@ -0,0 +1,109 @@
+"use strict";
+
+const moment = require('moment');
+
+const moduleName = 'analytics:accountsDeletedTestCommercialTotal';
+
+
+module.exports = {
+
+
+  friendlyName: 'analytics:accountsDeletedTestCommercialTotal',
+
+  description: 'Calculate data for accounts_deleted_test_commercial_total event',
+
+  inputs: {
+    start: {
+      friendlyName: 'start',
+      description: 'calculation interval start date & time',
+      type: 'string',
+      required: true,
+    },
+    end: {
+      friendlyName: 'end',
+      description: 'calculation interval end date & time',
+      type: 'string',
+      required: true,
+    },
+  },
+
+  exits: {
+
+    success: {
+      description: 'All done.',    },
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    sails.log.info(`******************** ${moduleName} at ${moment().format()} ********************`);
+
+    let elapsedTimeStart;
+    let elapsedTimeEnd;
+    let elapsedTime;
+    let numAccounts;
+    const serviceName = 'test_commercial';
+
+    try {
+
+      elapsedTimeStart = moment();
+
+      const serviceRec = await Service.findOne({
+        name: serviceName
+      });
+
+      if (serviceRec != null) {
+
+        numAccounts = await Account.count({
+          where: {
+            deleted: true,
+            service: serviceRec.id,
+          },
+        });
+
+        elapsedTimeEnd = moment();
+
+        elapsedTime = moment.duration(elapsedTimeEnd.diff(elapsedTimeStart)).asSeconds();
+
+        // sails.log.info(`Start: ${moment(inputs.start).format()}`);
+        // sails.log.info(`End: ${moment(inputs.end).format()}`);
+        // sails.log.info(`numAccounts: ${numAccounts}`);
+        // sails.log.info(`elapsedTime: ${elapsedTime}`);
+
+        return exits.success({
+          status: 'ok',
+          message: 'Success',
+          payload: {
+            value: numAccounts,
+            elapsedTime: elapsedTime,
+          }
+        });
+
+      } else {
+        throw new Error(`${moduleName}, error: No service with name="${serviceName}"`);
+      }
+
+
+    } catch (e) {
+
+      const errorLocation = moduleName;
+      const errorMsg = `${moduleName}: General error`;
+
+      sails.log.error(errorLocation + ', error: ' + errorMsg);
+      sails.log.error(errorLocation + ', error details: ', e);
+
+      throw {err: {
+          module: errorLocation,
+          message: errorMsg,
+          payload: {},
+        }
+      };
+
+    }
+
+  } // fn
+
+
+};
+
