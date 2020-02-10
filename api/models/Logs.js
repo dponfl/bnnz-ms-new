99fd225c1eb82d5ec861f79dee32558a905f0b85
dmsch <dschukin@mail.ru>
2020-02-10 21:56:54 +0200

    add logs table

---
+++ api/models/Logs.js
@@ -0,0 +1,52 @@
+/**
+ * Logs.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: 'tasks',
+  migrate: 'safe',
+  attributes: {
+
+    guid: {
+      type: 'string',
+      unique: true,
+    },
+    client_guid: {
+      type: 'string',
+    },
+    account_guid: {
+      type: 'string',
+    },
+    funnel: {
+      type: 'string',
+    },
+    action: {
+      type: 'string',
+    },
+    details: {
+      type: 'string',
+    },
+
+
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
