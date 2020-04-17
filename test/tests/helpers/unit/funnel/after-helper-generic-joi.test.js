bc0af4d1ab80341ae3974c36d0f9b0479ea88fc7
dmsch <dschukin@mail.ru>
2020-04-17 14:11:28 +0300

    implement sinon-chai and finalize after-helper-generic-joi tests

--- test/tests/helpers/unit/funnel/after-helper-generic-joi.test.js
+++ test/tests/helpers/unit/funnel/after-helper-generic-joi.test.js
@@ -1,12 +1,17 @@
 "use strict";
 
-const {expect} = require('chai');
+// const {expect} = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
 const sinon = require('sinon');
+const sinonChai = require("sinon-chai");
 const mlog = require('mocha-logger');
 const casual = require('casual');
 const clientSdk = require('../../../../sdk/client');
 
-describe.only('funnel.afterHelperGenericJoi test', function () {
+chai.use(sinonChai);
+
+describe('funnel.afterHelperGenericJoi test', function () {
 
   let customConfig, customConfigGeneral;
 
@@ -479,19 +484,214 @@ describe.only('funnel.afterHelperGenericJoi test', function () {
 
   });
 
-  // TODO: Использовать sinon-chai для проверки вызова хелпера с определёнными параметрами
+  describe('Perform helper', function () {
 
-  // describe('Check helper call', function () {
-  //
-  //   beforeEach(function () {
-  //
-  //   });
-  //
-  //   afterEach(function () {
-  //
-  //   });
-  //
-  // });
+    let client;
+    let clientUpdateJoiStub;
+
+    beforeEach(async function () {
+      client = await clientSdk.generateClient({current_funnel: 'optin'});
+      client.funnels = {
+        optin: [
+          {
+            "id": "step01",
+            "description": "",
+            "actionType": "text",
+            "initial": true,
+            "enabled": true,
+            "show_time": 0,
+            "previous": null,
+            "next": "optin::step02",
+            "switchToFunnel": null,
+            "beforeHelper": null,
+            "afterHelper": null,
+            "forcedHelper": null,
+            "callbackHelper": null,
+            "blockModifyHelper": null,
+            "shown": false,
+            "done": false,
+            "message": {
+              "html": [
+                {
+                  "text": "MSG_STEP01",
+                  "style": "",
+                  "cr": ""
+                }
+              ]
+            }
+          },
+          {
+            "id": "step02",
+            "description": "",
+            "actionType": "text",
+            "initial": false,
+            "enabled": false,
+            "show_time": 2000,
+            "previous": "optin::step01",
+            "next": "optin::step03",
+            "switchToFunnel": null,
+            "beforeHelper": null,
+            "afterHelper": null,
+            "forcedHelper": null,
+            "callbackHelper": null,
+            "blockModifyHelper": null,
+            "shown": false,
+            "done": false,
+            "message": {
+              "html": [
+                {
+                  "text": "MSG_STEP02",
+                  "style": "b",
+                  "cr": ""
+                }
+              ]
+            }
+          },
+          {
+            "id": "step03",
+            "description": "",
+            "actionType": "text",
+            "initial": false,
+            "enabled": false,
+            "show_time": 5000,
+            "previous": "optin::step02",
+            "next": "general::step04",
+            "switchToFunnel": null,
+            "beforeHelper": null,
+            "afterHelper": null,
+            "forcedHelper": null,
+            "callbackHelper": null,
+            "blockModifyHelper": null,
+            "shown": false,
+            "done": false,
+            "message": {
+              "html": [
+                {
+                  "text": "MSG_STEP03",
+                  "style": "i",
+                  "cr": ""
+                }
+              ]
+            }
+          },
+        ],
+        general: [
+          {
+            "id": "step04",
+            "description": "",
+            "actionType": "doc",
+            "initial": false,
+            "enabled": false,
+            "show_time": 3000,
+            "previous": "optin::step03",
+            "next": "general::step05",
+            "switchToFunnel": null,
+            "beforeHelper": null,
+            "afterHelper": null,
+            "forcedHelper": null,
+            "callbackHelper": null,
+            "blockModifyHelper": null,
+            "shown": false,
+            "done": false,
+            "message": {
+              "doc": "v1586616265/ABC123.pdf",
+              "html": [
+                {
+                  "text": "MSG_STEP04",
+                  "style": "b",
+                  "cr": ""
+                }
+              ]
+            }
+          },
+          {
+            "id": "step05",
+            "description": "",
+            "actionType": "text",
+            "initial": false,
+            "enabled": false,
+            "show_time": 3000,
+            "previous": "general::step04",
+            "next": null,
+            "switchToFunnel": null,
+            "beforeHelper": null,
+            "afterHelper": null,
+            "forcedHelper": null,
+            "callbackHelper": null,
+            "blockModifyHelper": null,
+            "shown": false,
+            "done": false,
+            "message": {
+              "html": [
+                {
+                  "text": "MSG_STEP05",
+                  "style": "",
+                  "cr": ""
+                }
+              ]
+            }
+          },
+        ]
+      };
+      clientUpdateJoiStub = sinon.stub(sails.helpers.storage, 'clientUpdateJoi');
+    });
+
+    afterEach(function () {
+      client = null;
+      clientUpdateJoiStub.restore();
+    });
+
+    it('should successfully perform the helper', async function () {
+
+      const funnel = 'optin';
+      const blockId = 'step02';
+      const block = _.find(client.funnels[funnel], {id: blockId});
+      if (_.isNil(block)) {
+        expect.fail(`funnel block for id: ${blockId} not found`);
+      }
+      block.switchToFunnel = 'general';
+      const next = true;
+      const previous = true;
+      const switchFunnel = true;
+
+      const initialBlock = block;
+
+      const params = {
+        client,
+        block,
+        next,
+        previous,
+        switchFunnel,
+      };
+
+      expect(client.funnels.optin[2]).to.deep.include({
+        enabled: false
+      });
+      expect(client.funnels.optin[0]).to.deep.include({
+        done: false
+      });
+      expect(client).to.deep.include({
+        current_funnel: 'optin'
+      });
+      await sails.helpers.funnel.afterHelperGenericJoi(params);
+      expect(clientUpdateJoiStub.callCount).to.be.eq(1);
+      expect(clientUpdateJoiStub).to.have.been.calledWith({
+        criteria: {guid: client.guid},
+        data: client,
+      });
+      expect(client.funnels.optin[2]).to.deep.include({
+        enabled: true
+      });
+      expect(client.funnels.optin[0]).to.deep.include({
+        done: true
+      });
+      expect(client).to.deep.include({
+        current_funnel: 'general'
+      });
+
+    });
+
+  });
 
 });
 
