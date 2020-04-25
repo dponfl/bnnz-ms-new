ed4d41a0d410731d544c28b5f055c9fa061b1d62
dmsch <dschukin@mail.ru>
2020-04-25 18:09:16 +0300

    after-try-again, config & funnels

---
+++ api/helpers/funnel/silver-personal/optin/after-try-again.js
@@ -0,0 +1,183 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'funnel:silver-personal:optin:after-try-again';
+
+
+module.exports = {
+
+
+  friendlyName: 'funnel:silver-personal:optin:after-try-again',
+
+
+  description: 'funnel:silver-personal:optin:after-try-again',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      client: Joi
+        .any()
+        .description('Client record')
+        .required(),
+      block: Joi
+        .any()
+        .description('Current funnel block')
+        .required(),
+      msg: Joi
+        .any()
+        .description('Message received'),
+    });
+
+    let input;
+
+    try {
+
+      input = await schema.validateAsync(inputs.params);
+
+      let updateBlock;
+      let getBlock;
+      let splitRes;
+      let updateFunnel;
+      let updateId;
+
+      /**
+       * Update optin::get_profile block
+       */
+
+      updateBlock = 'optin::get_profile';
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+      if (_.isNil(updateFunnel)
+        || _.isNil(updateId)
+      ) {
+        throw new Error(`${moduleName}, error: parsing error of ${updateBlock}`);
+      }
+
+      getBlock = _.find(input.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.shown = false;
+        getBlock.done = false;
+        getBlock.next = null;
+      } else {
+        throw new Error(`${moduleName}, error: could not find block with id: ${updateId} at: \n${input.client.funnels[updateFunnel]}`);
+      }
+
+      /**
+       * Update optin::confirm_profile block
+       */
+
+      updateBlock = 'optin::confirm_profile';
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+      if (_.isNil(updateFunnel)
+        || _.isNil(updateId)
+      ) {
+        throw new Error(`${moduleName}, error: parsing error of ${updateBlock}`);
+      }
+
+      getBlock = _.find(input.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.enabled = false;
+        getBlock.shown = false;
+        getBlock.done = false;
+        getBlock.next = null;
+      } else {
+        throw new Error(`${moduleName}, error: could not find block with id: ${updateId} at: \n${input.client.funnels[updateFunnel]}`);
+      }
+
+      /**
+       * Update optin::wrong_profile block
+       */
+
+      updateBlock = 'optin::wrong_profile';
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+      if (_.isNil(updateFunnel)
+        || _.isNil(updateId)
+      ) {
+        throw new Error(`${moduleName}, error: parsing error of ${updateBlock}`);
+      }
+
+      getBlock = _.find(input.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.enabled = false;
+        getBlock.shown = false;
+        getBlock.done = false;
+      } else {
+        throw new Error(`${moduleName}, error: could not find block with id: ${updateId} at: \n${input.client.funnels[updateFunnel]}`);
+      }
+
+      await sails.helpers.funnel.afterHelperGenericJoi({
+        client: input.client,
+        block: input.block,
+        msg: input.msg,
+        next: true,
+        previous: false,
+        switchFunnel: false,
+      });
+
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {},
+      })
+
+    } catch (e) {
+
+      const errorLocation = moduleName;
+      const errorMsg = `${moduleName}: General error`;
+
+      sails.log.error(errorLocation + ', error: ' + errorMsg);
+      sails.log.error(errorLocation + ', error details: ', e);
+
+      throw {err: {
+          module: errorLocation,
+          message: errorMsg,
+          payload: {
+            error: e,
+          },
+        }
+      };
+
+    }
+
+  }
+
+};
+
