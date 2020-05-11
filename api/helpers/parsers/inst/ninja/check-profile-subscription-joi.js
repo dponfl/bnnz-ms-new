65bf1eb6e7038ed1fa82146c42c6b7219e8f5da3
dmsch <dschukin@mail.ru>
2020-05-11 12:11:13 +0300

    check-profile-subscription-joi dummy implementation

---
+++ api/helpers/parsers/inst/ninja/check-profile-subscription-joi.js
@@ -0,0 +1,106 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'parsers:inst:ninja:check-profile-subscription-joi';
+
+
+module.exports = {
+
+
+  friendlyName: 'parsers:inst:ninja:check-profile-subscription-joi',
+
+
+  description: 'Проверка, что один профиль подписан на другие профили',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      checkProfile: Joi
+        .string()
+        .description('Instagram profile to check subscription')
+        .required(),
+      profilesList: Joi
+        .any()
+        .description('list of Instagram profile to check subscription on')
+        .required(),
+      flag: Joi
+        .number()
+        .description('number of profiles to decline'),
+    });
+
+
+    let result;
+
+    try {
+
+      const input = await schema.validateAsync(inputs.params);
+
+      /**
+       * Заглушка (должна быть заменена на обращение к API парсера
+       */
+
+      const notSubscribed = [];
+      const subscribed = input.profilesList;
+
+      for (let i = 0; i < input.flag; i++) {
+        const randomIndex = _.random(0, input.profilesList.length - 1);
+        notSubscribed.push(input.profilesList[randomIndex]);
+        _.pull(subscribed, input.profilesList[randomIndex]);
+      }
+
+      result = {
+        allSubscribed: input.flag === 0,
+        notSubscribed,
+        subscribed,
+      };
+
+
+      return exits.success(result);
+
+    } catch (e) {
+
+      const errorLocation = moduleName;
+      const errorMsg = `${moduleName}: General error`;
+
+      sails.log.error(errorLocation + ', error: ' + errorMsg);
+      sails.log.error(errorLocation + ', error details: ', e);
+
+      throw {err: {
+          module: errorLocation,
+          message: errorMsg,
+          payload: {
+            error: e,
+          },
+        }
+      };
+
+    }
+
+  }
+
+};
+
