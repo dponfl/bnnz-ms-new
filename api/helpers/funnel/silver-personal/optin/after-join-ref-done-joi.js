e0f0e957760e44feed01adf64ac915271b78703a
dmsch <dschukin@mail.ru>
2020-05-12 16:32:18 +0300

    join_ref_done

---
+++ api/helpers/funnel/silver-personal/optin/after-join-ref-done-joi.js
@@ -0,0 +1,135 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'funnel:silver-personal:optin:after-join-ref-done-joi';
+
+
+module.exports = {
+
+
+  friendlyName: 'funnel:silver-personal:optin:after-join-ref-done-joi',
+
+
+  description: 'funnel:silver-personal:optin:after-join-ref-done-joi',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      client: Joi
+        .any()
+        .description('Client record')
+        .required(),
+      block: Joi
+        .any()
+        .description('Current funnel block')
+        .required(),
+      msg: Joi
+        .any()
+        .description('Message received'),
+    });
+
+    let input;
+
+    try {
+
+      input = await schema.validateAsync(inputs.params);
+
+      let updateBlock;
+      let getBlock;
+      let splitRes;
+      let updateFunnel;
+      let updateId;
+
+      /**
+       * Update next block
+       */
+
+      updateBlock = input.block.next;
+
+      splitRes = _.split(updateBlock, sails.config.custom.JUNCTION, 2);
+      updateFunnel = splitRes[0];
+      updateId = splitRes[1];
+
+      if (_.isNil(updateFunnel)
+        || _.isNil(updateId)
+      ) {
+        throw new Error(`${moduleName}, error: parsing error of ${updateBlock}`);
+      }
+
+      getBlock = _.find(input.client.funnels[updateFunnel], {id: updateId});
+
+      if (getBlock) {
+        getBlock.shown = false;
+        getBlock.done = false;
+        getBlock.previous = 'optin::join_ref_done';
+      } else {
+        throw new Error(`${moduleName}, error: block not found:
+             updateBlock: ${updateBlock}
+             updateFunnel: ${updateFunnel}
+             updateId: ${updateId}
+             input.client.funnels[updateFunnel]: ${JSON.stringify(input.client.funnels[updateFunnel], null, 3)}`);
+      }
+
+      await sails.helpers.funnel.afterHelperGenericJoi({
+        client: input.client,
+        block: input.block,
+        msg: input.msg,
+        next: true,
+        previous: true,
+        switchFunnel: true,
+        createdBy: moduleName,
+      });
+
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {},
+      })
+
+    } catch (e) {
+
+      const errorLocation = moduleName;
+      const errorMsg = `${moduleName}: General error`;
+
+      sails.log.error(errorLocation + ', error: ' + errorMsg);
+      sails.log.error(errorLocation + ', error details: ', e);
+
+      throw {err: {
+          module: errorLocation,
+          message: errorMsg,
+          payload: {
+            error: e,
+          },
+        }
+      };
+
+    }
+
+  }
+
+};
+
