4b3a35d7b37f2e5bd373f67ec68d999aaba021a7
dmsch <dschukin@mail.ru>
2020-05-19 22:08:18 +0300

    after-can-provide-next-post-link-joi

---
+++ api/helpers/funnel/silver-personal/main/after-can-provide-next-post-link-joi.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'funnel:silver-personal:main:after-can-provide-next-post-link-joi';
+
+
+module.exports = {
+
+
+  friendlyName: 'funnel:silver-personal:main:after-can-provide-next-post-link-joi',
+
+
+  description: 'funnel:silver-personal:main:after-can-provide-next-post-link-joi',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      client: Joi
+        .any()
+        .description('Client record')
+        .required(),
+      block: Joi
+        .any()
+        .description('Current funnel block')
+        .required(),
+      msg: Joi
+        .any()
+        .description('Message received'),
+    });
+
+    let input;
+
+    try {
+
+      input = await schema.validateAsync(inputs.params);
+
+      const currentAccount = _.find(input.client.accounts, {guid: input.client.account_use});
+      const currentAccountInd = _.findIndex(input.client.accounts, (o) => {
+        return o.guid === currentAccount.guid;
+      });
+
+      input.block.done = true;
+
+      /**
+       * Update General funnel to the initial state to enable the client to perform it again
+       */
+
+      await sails.helpers.general.loadInitialFunnelsJoi({
+        client: input.client,
+        clientCategory: input.client.accounts[currentAccountInd]['service']['funnel_name'],
+        funnelName: 'main',
+      });
+
+      await sails.helpers.funnel.afterHelperGenericJoi({
+        client: input.client,
+        block: input.block,
+        msg: input.msg,
+        next: true,
+        previous: false,
+        switchFunnel: true,
+        createdBy: moduleName,
+      });
+
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {},
+      })
+
+    } catch (e) {
+
+      const errorLocation = moduleName;
+      const errorMsg = `${moduleName}: General error`;
+
+      sails.log.error(errorLocation + ', error: ' + errorMsg);
+      sails.log.error(errorLocation + ', error details: ', e);
+
+      throw {err: {
+          module: errorLocation,
+          message: errorMsg,
+          payload: {
+            error: e,
+          },
+        }
+      };
+
+    }
+
+  }
+
+};
+
