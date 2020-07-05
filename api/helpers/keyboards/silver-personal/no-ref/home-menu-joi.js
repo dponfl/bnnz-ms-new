fefc467d15b7c6de798d7d90d3a857299dd7d712
dmsch <dschukin@mail.ru>
2020-07-05 22:43:13 +0300

    NoRef Menu: keyboards helpers

---
+++ api/helpers/keyboards/silver-personal/no-ref/home-menu-joi.js
@@ -0,0 +1,108 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'keyboards:silver-personal:no-ref:home-menu-joi';
+
+
+module.exports = {
+
+
+  friendlyName: 'keyboards:silver-personal:no-ref:home-menu-joi',
+
+
+  description: 'keyboards:silver-personal:no-ref:home-menu-joi',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      client: Joi
+        .any()
+        .description('client object')
+        .required(),
+    });
+
+    let input;
+
+    try {
+
+      input = await schema.validateAsync(inputs.params);
+
+      const currentAccount = _.find(input.client.accounts, {guid: input.client.account_use});
+
+      currentAccount.keyboard = "home::start";
+
+      await sails.helpers.storage.clientUpdateJoi({
+        criteria: {guid: input.client.guid},
+        data: input.client,
+        createdBy: moduleName,
+      });
+
+      const sendKeyboardForAccountParams = {
+        client: input.client,
+      };
+
+      const sendKeyboardForAccountRaw = await sails.helpers.keyboardProcessor.sendKeyboardForAccountJoi(sendKeyboardForAccountParams);
+
+      if (sendKeyboardForAccountRaw.status !== 'ok') {
+        throw new Error(`${moduleName}, error: wrong sendKeyboardForAccountJoi response
+        sendKeyboardForAccountParams: ${JSON.stringify(sendKeyboardForAccountParams, null, 3)}
+        sendKeyboardForAccountRaw: ${JSON.stringify(sendKeyboardForAccountRaw, null, 3)}`);
+      }
+
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {},
+      })
+
+    } catch (e) {
+
+      const errorMsg = 'General error';
+
+      sails.log.error(`${moduleName}, Error details:
+      Platform error message: ${errorMsg}
+      Error name: ${e.name || 'no name'}
+      Error message: ${e.message || 'no message'}
+      Error stack: ${JSON.stringify(e.stack || {}, null, 3)}`);
+
+      throw {err: {
+          module: `${moduleName}`,
+          message: errorMsg,
+          payload: {
+            error_name: e.name || 'no name',
+            error_message: e.message || 'no message',
+            error_stack: e.stack || {},
+          },
+        }
+      };
+
+    }
+
+  }
+
+};
+
---
+++ api/helpers/keyboards/silver-personal/no-ref/join-ref-joi.js
@@ -0,0 +1,136 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'keyboards:silver-personal:no-ref:join-ref-joi';
+
+
+module.exports = {
+
+
+  friendlyName: 'keyboards:silver-personal:no-ref:join-ref-joi',
+
+
+  description: 'keyboards:silver-personal:no-ref:join-ref-joi',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      client: Joi
+        .any()
+        .description('client object')
+        .required(),
+    });
+
+    let input;
+    let currentAccount;
+
+    try {
+
+      input = await schema.validateAsync(inputs.params);
+
+      currentAccount = _.find(input.client.accounts, {guid: input.client.account_use});
+      const currentAccountInd = _.findIndex(input.client.accounts, (o) => {
+        return o.guid === currentAccount.guid;
+      });
+
+      /**
+       * Update General funnel to the initial state to enable the client to perform it again
+       */
+
+      const loadInitialFunnelsJoiParams = {
+        client: input.client,
+        clientCategory: input.client.accounts[currentAccountInd]['service']['funnel_name'],
+        funnelName: 'noRefJoin',
+      };
+
+      const loadInitialFunnelsJoiRaw = await sails.helpers.general.loadInitialFunnelsJoi(loadInitialFunnelsJoiParams);
+
+      if (loadInitialFunnelsJoiRaw.status !== 'ok') {
+        throw new Error(`${moduleName}, error: wrong loadInitialFunnelsJoi response:
+              loadInitialFunnelsJoiParams: ${JSON.stringify(loadInitialFunnelsJoiParams, null, 3)}
+              loadInitialFunnelsJoiRaw: ${JSON.stringify(loadInitialFunnelsJoiRaw, null, 3)}`);
+      }
+
+      input.client = loadInitialFunnelsJoiRaw.payload.client;
+
+      currentAccount = _.find(input.client.accounts, {guid: input.client.account_use});
+
+      currentAccount.keyboard = null;
+
+      /**
+       * Установить в client, что выполняется воронка "noRefJoin"
+       */
+
+      input.client.current_funnel = 'noRefJoin';
+
+      const initialBlock = _.find(input.client.funnels[input.client.current_funnel],
+        {initial: true});
+
+      initialBlock.enabled = true;
+
+      await sails.helpers.funnel.proceedNextBlockJoi({
+        client: input.client,
+        funnelName: input.client.current_funnel,
+        blockId: "join_ref_prepare_list",
+        createdBy: moduleName,
+      });
+
+
+
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {},
+      })
+
+    } catch (e) {
+
+      const errorMsg = 'General error';
+
+      sails.log.error(`${moduleName}, Error details:
+      Platform error message: ${errorMsg}
+      Error name: ${e.name || 'no name'}
+      Error message: ${e.message || 'no message'}
+      Error stack: ${JSON.stringify(e.stack || {}, null, 3)}`);
+
+      throw {err: {
+          module: `${moduleName}`,
+          message: errorMsg,
+          payload: {
+            error_name: e.name || 'no name',
+            error_message: e.message || 'no message',
+            error_stack: e.stack || {},
+          },
+        }
+      };
+
+    }
+
+  }
+
+};
+
