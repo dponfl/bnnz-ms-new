5f11adea95f024a312c989dea8e34ea13b9baf58
dmsch <dschukin@mail.ru>
2020-07-11 18:28:58 +0300

    update dummy checkProfileExistsJoi

--- api/helpers/parsers/inst/dummy/check-profile-exists-joi.js
+++ api/helpers/parsers/inst/dummy/check-profile-exists-joi.js
@@ -59,10 +59,22 @@ module.exports = {
        * Заглушка (должна быть заменена на обращение к API парсера
        */
 
-      profileExists = true;
+      const userPk = "25025320";
+      const profileExists = !!userPk;
+      const profileId = "25025320";
+      const profileFullName = "Instagram";
+      const profilePicUrl = "https://scontent-arn2-1.cdninstagram.com/v/t51.2885-19/s150x150/101703932_264332984810770_2870985139712688128_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_ohc=JD-tA9Oeep8AX_e6dln&oh=b032c0b0eebad1109ce46d31ebcc80b9&oe=5F347BFD";
 
-
-      return exits.success(profileExists);
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {
+          profileExists,
+          profileId,
+          profileFullName,
+          profilePicUrl,
+        },
+      })
 
     } catch (e) {
 
