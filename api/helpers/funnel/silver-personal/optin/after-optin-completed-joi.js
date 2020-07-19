d3268785ab4059d3a358cf6551dc33975bd91a84
dmsch <dschukin@mail.ru>
2020-07-19 21:05:01 +0300

    bug fix re. Optin final block display

---
+++ api/helpers/funnel/silver-personal/optin/after-optin-completed-joi.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'funnel:silver-personal:optin:after-optin-completed-joi';
+
+
+module.exports = {
+
+
+  friendlyName: 'funnel:silver-personal:optin:after-optin-completed-joi',
+
+
+  description: 'funnel:silver-personal:optin:after-optin-completed-joi',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      client: Joi
+        .any()
+        .description('Client record')
+        .required(),
+      block: Joi
+        .any()
+        .description('Current funnel block')
+        .required(),
+      msg: Joi
+        .any()
+        .description('Message received'),
+    });
+
+    let input;
+
+    try {
+
+      input = await schema.validateAsync(inputs.params);
+
+
+      await sails.helpers.funnel.afterHelperGenericJoi({
+        client: input.client,
+        block: input.block,
+        msg: input.msg,
+        next: true,
+        previous: true,
+        switchFunnel: true,
+        createdBy: moduleName,
+      });
+
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {},
+      })
+
+    } catch (e) {
+
+      const errorMsg = 'General error';
+
+      sails.log.error(`${moduleName}, Error details:
+      Platform error message: ${errorMsg}
+      Error name: ${e.name || 'no name'}
+      Error message: ${e.message || 'no message'}
+      Error stack: ${JSON.stringify(e.stack || {}, null, 3)}`);
+
+      throw {err: {
+          module: `${moduleName}`,
+          message: errorMsg,
+          payload: {
+            error_name: e.name || 'no name',
+            error_message: e.message || 'no message',
+            error_stack: e.stack || {},
+          },
+        }
+      };
+
+    }
+
+  }
+
+};
+
