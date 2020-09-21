e9e3c7e17e5d936a4b3ebf92a5ea544e717e1467
dmsch <dschukin@mail.ru>
2020-09-21 21:44:36 +0300

    generic:befree-p: bug fix

--- api/helpers/funnel/generic/options/callback-be-free-personal-info-joi.js
+++ api/helpers/funnel/generic/options/callback-be-free-personal-info-joi.js
@@ -144,6 +144,7 @@ module.exports = {
                 switchToFunnel: null,
               },
             ],
+            proceedNextBlock: false,
             createdBy: moduleName,
           };
 
