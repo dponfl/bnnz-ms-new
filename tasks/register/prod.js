eff75b089f6fb01a4f42d0c1adf4eee6e82fd177
dmsch <dschukin@mail.ru>
2020-10-17 17:01:09 +0300

    node & sailsjs update plus necessary fixes

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -13,9 +13,7 @@
  */
 module.exports = function(grunt) {
   grunt.registerTask('prod', [
-    'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
     'compileAssets',
-    'babel',         //« Remove this to skip transpilation in production (not recommended)
     'concat',
     'uglify',
     'cssmin',
