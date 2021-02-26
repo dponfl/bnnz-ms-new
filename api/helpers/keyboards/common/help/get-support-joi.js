dd31badac2b372a93a14a1531e66f7e90e738e7d
dmsch <dschukin@mail.ru>
2021-02-26 13:07:04 +0200

    help buttons activation & support btn: started

---
+++ api/helpers/keyboards/common/help/get-support-joi.js
@@ -0,0 +1,154 @@
+"use strict";
+
+const Joi = require('@hapi/joi');
+
+const moduleName = 'keyboards:common:help:get-support-joi';
+
+
+module.exports = {
+
+
+  friendlyName: 'keyboards:common:help:get-support-joi',
+
+
+  description: 'keyboards:common:help:get-support-joi',
+
+
+  inputs: {
+
+    params: {
+      friendlyName: 'input params',
+      description: 'input params',
+      type: 'ref',
+      required: true,
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'All done.',
+    },
+    err: {
+      description: 'Error',
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const schema = Joi.object({
+      client: Joi
+        .any()
+        .description('Client record')
+        .required(),
+    });
+
+    let input;
+
+    let client;
+    let clientGuid;
+    let accountGuid;
+
+
+    try {
+
+      input = await schema.validateAsync(inputs.params);
+
+      client = input.client;
+      clientGuid = client.guid;
+      accountGuid = client.account_use;
+
+
+      const currentAccount = _.find(client.accounts, {guid: client.account_use});
+
+      /**
+       * Достаём данные PushMessage
+       */
+
+      const pushMessageName = currentAccount.service.push_message_name;
+
+      const pushMessageGetParams = {
+        pushMessageName,
+      };
+
+      const pushMessageGetRaw = await sails.helpers.storage.pushMessageGetJoi(pushMessageGetParams);
+
+      if (pushMessageGetRaw.status !== 'ok') {
+        await sails.helpers.general.throwErrorJoi({
+          errorType: sails.config.custom.enums.errorType.ERROR,
+          location: moduleName,
+          message: 'Wrong pushMessageGetJoi response',
+          clientGuid,
+          accountGuid,
+          errorName: sails.config.custom.STORAGE_ERROR.name,
+          payload: {
+            pushMessageGetParams,
+            pushMessageGetRaw,
+          },
+        });
+
+      }
+
+      const pushMessage = pushMessageGetRaw.payload;
+
+      const messageDataPath = 'keyboards.help.getSupport';
+      const messageData = _.get(pushMessage, messageDataPath, null);
+
+      if (messageData == null) {
+        await sails.helpers.general.throwErrorJoi({
+          errorType: sails.config.custom.enums.errorType.ERROR,
+          location: moduleName,
+          message: 'No expected messageData',
+          clientGuid,
+          accountGuid,
+          errorName: sails.config.custom.STORAGE_ERROR.name,
+          payload: {
+            pushMessage,
+            messageDataPath,
+            messageData,
+          },
+        });
+      }
+
+      const msgRes = await sails.helpers.messageProcessor.sendMessageJoi({
+        client,
+        messageData,
+        disableWebPagePreview: true,
+      });
+
+      return exits.success({
+        status: 'ok',
+        message: `${moduleName} performed`,
+        payload: {},
+      })
+
+    } catch (e) {
+
+      const throwError = true;
+      if (throwError) {
+        return await sails.helpers.general.catchErrorJoi({
+          error: e,
+          location: moduleName,
+          throwError: true,
+        });
+      } else {
+        await sails.helpers.general.catchErrorJoi({
+          error: e,
+          location: moduleName,
+          throwError: false,
+        });
+        return exits.success({
+          status: 'ok',
+          message: `${moduleName} not performed`,
+          payload: {},
+        });
+      }
+
+    }
+
+  }
+
+};
+
